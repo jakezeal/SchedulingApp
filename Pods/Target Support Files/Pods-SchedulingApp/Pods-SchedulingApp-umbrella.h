934484f8ee493a4a4fb517862805bbdb76950f16
Jake Zeal <jake@jakezeal.com>
2016-04-21 13:36:57 -0400

    First commit to start collaborating.

---
+++ Pods/Target Support Files/Pods-SchedulingApp/Pods-SchedulingApp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SchedulingAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SchedulingAppVersionString[];
+
---
+++ SchedulingApp/bridge.h
@@ -0,0 +1,9 @@
+//
+//  bridge.h
+//  SchedulingApp
+//
+//  Created by Zeal on 4/21/16.
+//  Copyright © 2016 Jake Zeal. All rights reserved.
+//
+
+#import <FSCalendar/FSCalendar.h>
