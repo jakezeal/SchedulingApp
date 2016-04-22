605229f8b54b347674e3b7f7adda235024d0d1c4
ipjeffr <ip.jeff@gmail.com>
2016-04-22 12:36:39 -0400

    began implementing detailsVC for calendar

---
+++ SchedulingApp/CalendarDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CalendarDetailsViewController.swift
+//  SchedulingApp
+//
+//  Created by Tenzin Phagdol on 2016-04-22.
+//  Copyright © 2016 Jake Zeal. All rights reserved.
+//
+
+import UIKit
+
+class CalendarDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
