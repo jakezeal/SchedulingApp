9af6490524027adf1fb114cf0d1cd6501a2a7af8
ipjeffr <ip.jeff@gmail.com>
2016-04-25 13:45:06 -0400

    querying parse to get calendars for user

--- SchedulingApp/CalendarCollectionViewController.swift
+++ SchedulingApp/CalendarCollectionViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Parse
 
 class CalendarCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    var imageView = UIImageView()
+    var calendarNames: [String] = []
+    
     //MARK:- Outlets
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -17,6 +21,35 @@ class CalendarCollectionViewController: UIViewController, UICollectionViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         prepareCollectionView()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        //query parse for user-specific calendars
+        self.calendarNames = []
+        queryParse()
+    }
+    
+    func queryParse() {
+        let currentUser = PFUser.currentUser()
+        let query = PFQuery(className:"Calendar")
+        query.whereKey("usernames", equalTo: currentUser!["username"])
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?) -> Void in
+            if error == nil && objects != nil {
+                for object in objects! {
+                    
+                    let calendarTitle = object["title"] as! String
+                    self.calendarNames.append(calendarTitle)
+                    print(self.calendarNames)
+                    dispatch_async(dispatch_get_main_queue()){
+                        self.collectionView.reloadData()
+ 
+                    }
+                }
+            } else {
+                print(error)
+            }
+        }
     }
     
     //MARK:- Preperations
@@ -31,14 +64,16 @@ class CalendarCollectionViewController: UIViewController, UICollectionViewDelega
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return self.calendarNames.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cellIdentifier = "Cell"
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as! CalendarCollectionViewCell
         
-        cell.groupLabel.text = "Hello world"
+        self.imageView.frame = cell.frame
+        cell.addSubview(self.imageView)
+        self.imageView.image = UIImage.init(named:"calendar")
         
         return cell
     }
